9d351b4d2d0d1a2757ee49d1cafe12b759fbe472
Shamik1836 <dasshamik18@gmail.com>
2022-07-29 17:21:00 +0530

    Day 51 done

---
+++ Maximum_Sum_Increasing_Subsequence.java
@@ -0,0 +1,24 @@
+
+import java.util.ArrayList;
+
+public class Maximum_Sum_Increasing_Subsequence {
+    public static int maxIncreasingDumbbellsSum(ArrayList<Integer> rack, int n) {
+        // Write your code here.
+        int max=0;
+        int[] msis=new int[n];
+        
+        for (int i=0;i<n;i++){
+            msis[i]=rack.get(i);
+        }
+        for (int i=1;i<n;i++){
+            for (int j=0;j<i;j++){
+                if (rack.get(i)>rack.get(j) && msis[i]<msis[j]+rack.get(i))
+                    msis[i]=msis[j]+rack.get(i);
+            }
+        }
+        for (int i=0;i<n;i++)
+            if (max<msis[i])
+                max=msis[i];
+        return max;
+    }
+}
